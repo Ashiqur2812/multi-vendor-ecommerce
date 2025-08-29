112e79207acc75710b1cb4dceb2e3cc680920b92
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 02:38:25 +0600

    added the permission model

--- src/models/Permission.model/Permission.model.js
+++ src/models/Permission.model/Permission.model.js
@@ -2,10 +2,18 @@ const { Schema, model } = require("mongoose");
 
 const permissionSchema = new Schema(
   {
-    name: { type: String, unique: true, required: true, index: true },
+    name: {
+      type: String,
+      unique: true,
+      required: [true, 'Name must be required'],
+      index: true
+    },
     description: { type: String },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    versionKey: false
+  }
 );
 
 const Permission = model("Permission", permissionSchema);
