a2e6db96661808c70b9e66452606b88cf06704e4
Ashiqur <ashiqur2812@gmail.com>
2025-08-30 03:15:09 +0600

    polished the middleware section

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,20 +1,18 @@
 const express = require("express");
-const morgan = require("morgan"); // Middleware for logging HTTP requests
+const morgan = require("morgan"); 
 const compression = require("compression"); // Middleware for compressing HTTP responses
 const cookieParser = require("cookie-parser"); // Middleware for parsing cookies
 const helmet = require("helmet"); // Middleware for setting security-related HTTP headers
-const cors = require("cors"); // Middleware for providing a Connect/Express middleware that can be used to enable CORS with various options
+const cors = require("cors"); 
 const passport = require("passport");
 const expressSession = require("express-session");
 
 const host = require("../middleware/getHost");
 const config = require("../config/config");
-// const rateLimit = require("../utils/rateLimit.utils/rateLimit.utils");
-// const userAgent = require("../middleware/userAgent.middleware");
 
 const middleware = [
-  express.json({ limit: "16kb" }), // Middleware for parsing JSON request bodies with a size limit of 16kb
-  express.urlencoded({ extended: true, limit: "16kb" }), // Middleware for parsing URL-encoded request bodies with a size limit of 16kb
+  express.json({ limit: "16kb" }),
+  express.urlencoded({ extended: true, limit: "16kb" }),
   compression({
     level: 6,
     threshold: 100 * 1000,
@@ -31,7 +29,7 @@ const middleware = [
     resave: false,
     saveUninitialized: false,
   }),
-  morgan("dev"), // Morgan middleware for logging HTTP requests in the 'dev' format
+  morgan("dev"),
   helmet(), // Helmet middleware for setting security-related HTTP headers
   cors({
     origin: ["http://localhost:5173"], // Replace with your frontend origin
@@ -40,9 +38,6 @@ const middleware = [
   host,
   passport.initialize(),
   passport.session(),
-
-  // userAgent, // Custom middleware for parsing and handling user agents (commented out)
-  // rateLimit, // Custom middleware for rate limiting requests (commented out)
 ];
 
 module.exports = middleware;
